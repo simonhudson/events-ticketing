ec843fe91bf8279c78b9444a1b97fe643fa4ee06
Simon Hudson <hellosimonhudson@gmail.com>
2024-01-28 21:14:27 +0000

    use function in config

--- events/vite.config.ts
+++ events/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
 	plugins: [react()],
 	build: {
 		rollupOptions: {
-			external: ['./**/*.test.ts', './**/*.test.tsx'],
+			external: (id) => id.indexOf('.test.ts') > -1,
 		},
 	},
 });
