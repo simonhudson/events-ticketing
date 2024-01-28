a0887ea0c69e4804c8c2130cc128b0a3e6900330
Simon Hudson <hellosimonhudson@gmail.com>
2024-01-28 21:02:56 +0000

    update config

--- events/vite.config.ts
+++ events/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
 	plugins: [react()],
 	build: {
 		rollupOptions: {
-			external: ['*/**/*.test.ts', '*/**/*.test.tsx'],
+			external: ['./*/**/*.test.ts', './*/**/*.test.tsx'],
 		},
 	},
 });
