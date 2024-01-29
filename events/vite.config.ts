3ef36d9936af81656234a245d012989672539d51
Simon Hudson <hellosimonhudson@gmail.com>
2024-01-29 09:34:38 +0000

    fix build

--- events/vite.config.ts
+++ events/vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { fileURLToPath } from 'node:url';
+
+const filesNeedToExclude = ['*/**/.test.ts', '*/**/.test.tsx'];
+
+const filesPathToExclude = filesNeedToExclude.map((src) => {
+	return fileURLToPath(new URL(src, import.meta.url));
+});
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	build: {
+		manifest: true,
+		rollupOptions: {
+			external: [...filesPathToExclude],
+		},
+	},
 });
